83ba240211f4e6cf74aa9c62d210fe84a8429ebe
Tomoya Oda <tm_oda@eis.hokudai.ac.jp>
2021-11-30 01:35:10 +0900

    fix: MAX_MEM

--- cluster-smi.sh
+++ cluster-smi.sh
@@ -29,7 +29,7 @@ get_node() {
     RESULT="\t \t \t \n"
   else
     RESULT=$(echo "${NVIDIA_SMI}" | awk 'NR==31, NR==-2 {printf "%s\t%s\t%s\n",$2, $5, $8}')
-    MAX_MEM=$(echo "${NVIDIA_SMI}" | awk 'NR==7, NR==22 {print $11}' | awk 'NR%4==0 {print}')
+    MAX_MEM=$(echo "${NVIDIA_SMI}" | awk 'NR==7, NR==22 {print $11}' | awk 'NR%4==0 {print}' | head -n 1)
     VOLATILE=$(echo "${NVIDIA_SMI}" | awk 'NR==7, NR==22 {print $13}' | awk 'NR%4==0 {print}')
 
     for pid in ${PIDS}; do
@@ -41,8 +41,8 @@ get_node() {
     RESULT=$(echo "${RESULT}" | awk '{printf "%s / %s\n",$0 ,"_TOKEN_"}')
     # One by one
     line=1
-    for max_mem in ${MAX_MEM[@]}; do
-      RESULT=$(echo "${RESULT}" | sed "${line}s/_TOKEN_/${max_mem}/")
+    for pid in ${PIDS}; do
+      RESULT=$(echo "${RESULT}" | sed "${line}s/_TOKEN_/${MAX_MEM}/")
       let line++
     done
 
